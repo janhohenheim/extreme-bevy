439b710844e6cb9371210237b765c950a6bc3c1f
Jan Nils Ferner <contact@jnferner.com>
2022-05-15 16:09:58 +0200

    Fix error

--- src/networking/targets/shared.rs
+++ src/networking/targets/shared.rs
@@ -13,6 +13,7 @@ pub fn create_session_builder<GGRSConfig: Config>(
         .with_num_players(num_players)
         .with_max_prediction_window(20)
         .with_catchup_speed(2)
+        .expect("Invalid catchup speed")
         .with_fps(FPS)
         .expect("Invalid FPS")
         .with_input_delay(6)
